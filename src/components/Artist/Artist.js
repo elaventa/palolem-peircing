4e63ef35c41b3910bd28760d90785326c0f6364b
Rahul T <rahulmanojcoder@gmail.com>
2023-02-09 10:34:47 +0530

    initial commit

---
+++ src/components/Artist/Artist.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Artist.scss"
+
+const Artist = () => {
+  return (
+    <div>Artist</div>
+  )
+}
+
+export default Artist
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Navbar.scss"
+
+const Navbar = () => {
+  return (
+    <nav className="nav">Navbar</nav>
+  )
+}
+
+export default Navbar
