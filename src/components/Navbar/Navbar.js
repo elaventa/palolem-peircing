1cab2cc1b14160ebb062c06d1e20dc2d6e2fe3fe
Rahul T <rahulmanojcoder@gmail.com>
2023-02-09 23:36:45 +0530

    responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,16 @@
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import React from "react"
+import React, { useState } from "react"
+import { BsTelephone } from "react-icons/bs"
 import "./Navbar.scss"
 
 const Navbar = () => {
+  const [clicked, setclicked] = useState(false)
+  const handleClick = () => {
+    setclicked(!clicked)
+  }
   return (
-    <nav className="nav">
+    <nav className={`nav ${clicked}`}>
       <div className="logo">
         <StaticImage
           src="../../images/logo.png"
@@ -17,27 +22,49 @@ const Navbar = () => {
         />
       </div>
 
-      <ul className="navItems">
+      <ul className={`navItems ${clicked}`}>
         <Link to="#">
-          <li className="navItem">Home</li>
+          <li className="navItem" onClick={() => setclicked(false)}>
+            Home
+          </li>
         </Link>
 
         <Link to="#">
-          <li className="navItem">About</li>
+          <li className="navItem" onClick={() => setclicked(false)}>
+            About
+          </li>
         </Link>
 
         <Link to="#">
-          <li className="navItem">Services</li>
+          <li className="navItem" onClick={() => setclicked(false)}>
+            Services
+          </li>
         </Link>
 
         <Link to="#">
-          <li className="navItem">Gallery</li>
+          <li className="navItem" onClick={() => setclicked(false)}>
+            Gallery
+          </li>
         </Link>
 
         <Link to="#">
-          <li className="navItem">Contact</li>
+          <li className="navItem" onClick={() => setclicked(false)}>
+            Contact
+          </li>
         </Link>
       </ul>
+      <div className="mobileNav">
+        <a style={{ color: "#000" }} href="tel:+919921209571">
+          <div className="call">
+            <BsTelephone className="icon" />
+          </div>
+        </a>
+        <div onClick={handleClick} className={`hamburger ${clicked}`}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
     </nav>
   )
 }
