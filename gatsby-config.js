97183026edf41b49beb07f74c18ff5dc128f3d4f
Rahul T <rahulmanojcoder@gmail.com>
2023-02-14 00:24:07 +0530

    upate

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/whilte-logo-only.png`, // This path is relative to the root of the site.
+        icon: `src/images/white-logo-only.png`, // This path is relative to the root of the site.
       },
     },
   ],
