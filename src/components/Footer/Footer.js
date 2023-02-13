20c81cc7dee6408331b3ab2b1d83dfcb18d64ee1
Rahul T <rahulmanojcoder@gmail.com>
2023-02-13 23:51:42 +0530

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,33 +17,59 @@ const Footer = () => {
           />
         </div>
 
-    
-
-      <div className="category">
-        <h1>ARTIST PORTFOLIO</h1>
-       <a href=""> <li>Rutik KavdIiya </li><br/></a>
-        <h1>CATEGORIES</h1>
-        <a href=""> <li> Septum Piercing</li></a>
-        <a href=""> <li> Nose Peircing</li></a>
-        <a href=""> <li> Belly Peircing</li></a>
-      </div>
-      <div>
-        <h1>IMPORTANT LINKS</h1>
-        <a href=""><li> PeirIcing after care</li></a>
-        <a href=""> <li> Blog</li></a>
-        <a href=""><li> Privacy Policy</li></a>
-        <a href=""><li> Terms & Conditions</li></a>
-      </div>
-      <div>
-        <p> Palolem Main Street, Next to The Sun N Moon Wine Shop, Canacona, 403702, Goa, India <br />
-        <br />
-          +91 99212 09571 <br />
-          palolempiercingshop@gmail.com</p>
-        
-      </div>
-        
+        <div className="category">
+          <h1>ARTIST PORTFOLIO</h1>
+          <a href="">
+            {" "}
+            <li>Rutik KavdIiya </li>
+            <br />
+          </a>
+          <h1>CATEGORIES</h1>
+          <a href="">
+            {" "}
+            <li> Septum Piercing</li>
+          </a>
+          <a href="">
+            {" "}
+            <li> Nose Peircing</li>
+          </a>
+          <a href="">
+            {" "}
+            <li> Belly Peircing</li>
+          </a>
+        </div>
+        <div>
+          <h1>IMPORTANT LINKS</h1>
+          <a href="">
+            <li> PeirIcing after care</li>
+          </a>
+          <a href="">
+            {" "}
+            <li> Blog</li>
+          </a>
+          <a href="">
+            <li> Privacy Policy</li>
+          </a>
+          <a href="">
+            <li> Terms & Conditions</li>
+          </a>
+        </div>
+        <div>
+          <p>
+            {" "}
+            Palolem Main Street, Next to The Sun N Moon Wine Shop, Canacona,
+            403702, Goa, India <br />
+            <br />
+            <a href="tel:+919921209571">+91 99212 09571 </a> <br />
+            <a href="mailto:pravinkhavadiya@gmail.com">
+              pravinkhavadiya@gmail.com
+            </a>
+          </p>
+        </div>
       </section>
-      <p className="copyright">2023 All Rights Reserved. Palolem Piercing Shop Pvt Ltd.</p>
+      <p className="copyright">
+        2023 All Rights Reserved. Palolem Piercing Shop Pvt Ltd.
+      </p>
     </div>
   )
 }
--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -1,20 +1,20 @@
 import React from "react"
 import "./SocialIcons.scss"
 import { BsInstagram, BsWhatsapp } from "react-icons/bs"
-import { CiLocationOn } from "react-icons/ci"
+import { MdLocationPin } from "react-icons/md"
 const SocialIcons = () => {
   return (
     <div className="socialIcons">
       <a href="https://instagram.com/palolem_piercingshop">
-        <BsInstagram />
+        <BsInstagram  style={{color: "#E1306C", fontWeight: 'bolder', fontSize: '30'}} />
       </a>
 
       <a href="https://wa.me/+919921209571">
-        <BsWhatsapp />
+        <BsWhatsapp  style={{color: "#25D366", fontWeight: 'bolder', fontSize: '30'}} />
       </a>
 
       <a href="https://www.google.com/maps/place/Prav+Ink+Tattoos+Palolem/@15.0129304,74.0209363,17z/data=!3m1!4b1!4m5!3m4!1s0x3bbe45515d7120e5:0x317b01ded0976c6d!8m2!3d15.0129304!4d74.0231303?hl=en-IN&coh=164777&entry=tt&shorturl=1">
-        <CiLocationOn />
+        <MdLocationPin style={{color: "red", fontWeight: 'bolder', fontSize: '30'}} />
       </a>
     </div>
   )
